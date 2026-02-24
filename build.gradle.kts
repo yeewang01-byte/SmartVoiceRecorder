8e034306f40ed789919a05fb6b2d2e4b102a8c0a
wangyi <wangyi@wangyideMacBook-Pro-2.local>
2026-02-24 13:04:56 +0800

    fix(build): declare kotlin kapt plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     kotlin("android") version "1.9.0" apply false
+    kotlin("kapt") version "1.9.0" apply false
 }
 
 buildscript {
