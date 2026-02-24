fd3c357796dc90b486f6111b12b280128d160c68
wangyi <wangyi@wangyideMacBook-Pro-2.local>
2026-02-24 12:35:39 +0800

    fix: add pluginManagement repositories to settings.gradle.kts for Android plugin resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("com.android.library") version "8.1.0" apply false
     kotlin("android") version "1.9.0" apply false
